7be169faeedd23432661f58c38191b186dadde7a
adam-sagar <adamschrissagar@gmail.com>
2023-02-15 18:18:37 +1300

    Created two random dice

---
+++ js/numGenerator.js
@@ -0,0 +1,17 @@
+// let x = Math.floor((Math.random() * 6) + 1);
+
+// console.log(x)
+
+
+function D6() {
+    let x = Math.floor((Math.random() * 6) + 1);
+    document.getElementById("D6").innerHTML=x;
+}
+
+function D8() {
+    let x = Math.floor((Math.random() * 8) + 1);
+    document.getElementById("D8").innerHTML=x;
+}
+
+
+console.log(x)
