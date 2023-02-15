ff3782e7bfb2ada5151392df789044ed5720910b
adam-sagar <adamschrissagar@gmail.com>
2023-02-15 18:20:46 +1300

    Updated js file

--- js/numGenerator.js
+++ js/numGenerator.js
@@ -1,6 +1,6 @@
-// let x = Math.floor((Math.random() * 6) + 1);
+let x = Math.floor((Math.random() * 6) + 1);
 
-// console.log(x)
+console.log(x)
 
 
 function D6() {
@@ -12,6 +12,3 @@ function D8() {
     let x = Math.floor((Math.random() * 8) + 1);
     document.getElementById("D8").innerHTML=x;
 }
-
-
-console.log(x)
